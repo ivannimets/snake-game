946f74df13ce36f10efd1ebbff0f7c8c16320cfa
ivannimets <ivannimets@github.com>
2020-01-25 19:47:42 +0200

    created GameManager file

---
+++ Snake/GameManager.swift
@@ -0,0 +1,12 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by Німець Іван Олександрович on 25.01.2020.
+//  Copyright © 2020 Німець Іван Олександрович. All rights reserved.
+//
+
+import SpriteKit
+class GameManager {
+    
+}
