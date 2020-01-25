97038de886c9145566d5b82004b00dfe17bd0095
ivannimets <ivannimets@github.com>
2020-01-25 19:28:07 +0200

    finished main menu layout

--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -32,10 +32,32 @@ class GameScene: SKScene {
         
         gamelogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
         gamelogo.zPosition = 1
-        gamelogo.position = CGPoint(x: 0, y: (frame.size.height / 2)-200 )
+        gamelogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
         gamelogo.fontSize = 60
         gamelogo.text = "snake"
         gamelogo.fontColor = SKColor.red
         self.addChild(gamelogo)
+        
+        bestScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        bestScore.zPosition = 1
+        bestScore.position = CGPoint(x: 0, y: gamelogo.position.y - 50)
+        bestScore.fontSize = 40
+        bestScore.text = "Best Score : 0"
+        bestScore.fontColor = SKColor.white
+        self.addChild(bestScore)
+        
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 600)
+        playButton.fillColor = SKColor.white
+        let topCorner = CGPoint(x: -100, y: 200)
+        let bottomCorner = CGPoint(x: -100, y: 0)
+        let middle = CGPoint(x: 100, y: 100)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottomCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
     }
 }
